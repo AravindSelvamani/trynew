b3d907b8cdb1e2838de6744e2bab755dcfe2f8c3
AravindSelvamani <aravind.selvamani@freshworks.com>
2019-04-26 19:45:06 +0530

    partial the code and add delete method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,6 +36,13 @@
      @article = Article.find(params[:id])
    end
 
+   def destroy
+     @article = Article.find(params[:id])
+     @article.destroy
+     flash[:notice] = "Article is deleted successfully"
+     redirect_to articles_path
+   end
+
    private
     def article_params
       params.require(:article).permit(:title, :description)
